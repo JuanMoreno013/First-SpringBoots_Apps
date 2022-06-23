49e18aff220cd6ffaffa3853a9eb13f9a2f156e8
Juan Moreno <jmoreno@griddynamics.com>
2022-06-23 17:46:51 -0500

    First commit

---
+++ src/main/java/org/example/springRESTExample2/SpringRestExample2Application.java
@@ -0,0 +1,13 @@
+package org.example.springRESTExample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestExample2Application.class, args);
+	}
+
+}
